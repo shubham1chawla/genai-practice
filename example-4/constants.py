04469325ebadfc977f0e783a744789203253a0d0
Shubham Chawla <shubham_chawla@live.com>
2025-02-27 13:06:05 -0500

    example-4: improved graph structure

--- example-4/constants.py
+++ example-4/constants.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 BOOKS_JSON_PATH = './books.json'
 EXPORT_DIR = './debug/'
-CHUNK_SIZE = 800
-CHUNK_OVERLAP = 20
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 0
 LLM_MODEL_NAME = 'llama3.1:8b'
 NEO4J_URI = 'bolt://localhost:7687'
 NEO4J_AUTH = ('neo4j', os.environ['NEO4J_PASSWORD'])
-MAX_THREADS = 8
+MAX_WORKERS = 8
--- example-4/prompts.py
+++ example-4/prompts.py
@@ -9,13 +9,13 @@ EXTRACT_ENTITIES_RELATIONSHIPS_PROMPT = ChatPromptTemplate([
      1. Make sure that the name of the entities are proper nouns and does not contain adjectives, pronouns, and prepositions. 
      2. Only include entities which you think are important and relevant from the text.
      3. Avoid generic entities.
-     4. Entity Type should be in lowercase snake_case. For instance, 'parent_child', 'involved_in'.
+     4. Entity Type should be in lowercase snake_case.
      5. Name of entities should be lowercase.
      
      Important instructions on extracting relationships -
      1. Only extract relationships for entities you will be mentioning.
      2. Ensure you only include relationships between entities that you have extracted.
-     3. Relationship Type should be in lowercase snake_case. For instance, 'parent_child', 'involved_in'.
+     3. Relationship Type should be in lowercase snake_case.
      4. Name of entities 1 & 2 should match with the list of entities.
      5. Entities 1 & 2 both should be part of entities for them to have a relationship.
      6. Relationship is directed and defined from Entity 1 to Entity 2, i.e. e1 -> e2 
